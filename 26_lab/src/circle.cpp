8d40d408528e91ccd12f3e04f43c9d04cb0c8264
sophiabelkova <71042084+sophiabelkova@users.noreply.github.com>
2021-04-26 15:30:30 +0300

    Update circle.cpp

--- 26_lab/src/circle.cpp
+++ 26_lab/src/circle.cpp
@@ -1,1 +1,19 @@
-// Здесь код класса Circle
+class Circle: public Shape
+{
+    int radius;
+    
+public:
+    Circle(int rad)radius:(rad)
+    {
+    }
+
+    void draw() const override
+    {
+        for(int i=0;i<radius*2;i++)
+        {
+            // x = sqrt(r*r-y*y)
+            int len_x = std::sqrt(radius*radius-(radius-i)*(radius-i));
+            Shape::DrawConsoleLine(radius-len_x/2, len_x);
+        }
+    }    
+};
