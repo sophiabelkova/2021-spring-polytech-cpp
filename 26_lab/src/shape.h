3f9802f1b297d46dccca4c5226e45cfecef12e0b
sophiabelkova <71042084+sophiabelkova@users.noreply.github.com>
2021-04-26 15:27:58 +0300

    Update shape.h

--- 26_lab/src/shape.h
+++ 26_lab/src/shape.h
@@ -1,1 +1,19 @@
-// Здесь код класса Shape
+class Shape
+{
+    
+public:
+    
+    virtual void draw() const = 0; 
+
+    static void DrawConsoleLine(unsigned int x, unsigned int len)
+    {
+        
+        for(int i=0;i<x;++i)
+            std::cout <<' ';
+
+        for(int i=0;i<len;++i)
+            std::cout << '*';
+            
+        std::cout << std::endl;
+    }
+};
